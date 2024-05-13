2386ee31a1226bd5e1479406de13642f696e1216
Tunglies <Tunglies@outlook.com>
2024-05-13 18:38:24 +0800

    fix cannot find eth0

--- zsh/.customize.zsh
+++ zsh/.customize.zsh
@@ -1,13 +1,11 @@
 #!/bin/zsh
 
-ip_address=$(ip addr show eth0 | awk '/inet / {print $2}' | cut -d'/' -f1)
-gateway=$(ip route show dev eth0 | awk '/default via/ {print $3}')
-
-if [ -z "$ip_address" ] || [ -z "$gateway" ]; then
-    echo "Failed to get IP address or gateway of eth0"
-fi
-if [[ "${ip_address%%.*}" == "172" ]]; then
-    proxy_gateway="${ip_address%.*}.1"
+if ip link show eth0 &> /dev/null; then
+    ip_address=$(ip addr show eth0 | awk '/inet / {print $2}' | cut -d'/' -f1)
+    gateway=$(ip route show dev eth0 | awk '/default via/ {print $3}')
+    if [[ "${ip_address%%.*}" == "172" ]]; then
+        proxy_gateway="${ip_address%.*}.1"
+    fi
 else
     proxy_gateway="127.0.0.1"
 fi
@@ -16,8 +14,11 @@ export all_proxy="http://$proxy_gateway:7890"
 export PATH="/usr/sbin:$PATH";
 export PATH="/usr/lib/ccache/:$PATH"
 export PATH="/opt/riscv64-unknown-elf-gcc-8.3.0-2020.04.1-x86_64-linux-ubuntu14/bin:$PATH"
+export PATH="/opt/qemu-9.0.0/build:$PATH"
+export PATH="/opt/qemu-9.0.0/build/riscv64-sofmmu:$PATH"
+export PATH="/opt/qemu-9.0.0/build/rscv64-linux-user:$PATH"
 
-alias vim="nvim"
+#alias vim="nvim"
 alias cls="clear"
 
 alias gp="git push"
@@ -33,7 +34,4 @@ alias mc="make clean"
 
 if [ -z "$TMUX" ]; then
     tmux attach || tmux new-session
-fi
-if [ "$TMUX" == "" ]; then
-    cd ~
 fi
