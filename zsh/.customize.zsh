4e295dc68c0622816ab41a16f87998ca785fc133
debian <debian@debian.debian.local>
2024-05-13 17:26:09 +0800

    cargo mironor support

--- zsh/.customize.zsh
+++ zsh/.customize.zsh
@@ -1,10 +1,8 @@
-export all_proxy="http://172.25.112.1:7890"
+export all_proxy="http://127.0.0.1:7890"
 
+export PATH="/usr/sbin:$PATH";
 export PATH="/usr/lib/ccache/:$PATH"
-
-export PATH="$HOME/.os-env/qemu-8.2.0/build:$PATH"
-export PATH="$HOME/.os-env/qemu-8.2.0/build/riscv64-softmmu:$PATH"
-export PATH="$HOME/.os-env/qemu-8.2.0/build/riscv64-linux-user:$PATH"
+export PATH="/opt/riscv64-unknown-elf-gcc-8.3.0-2020.04.1-x86_64-linux-ubuntu14/bin:$PATH"
 
 alias vim="nvim"
 alias cls="clear"
@@ -15,6 +13,8 @@ alias ga="git commit -a -m"
 alias gc="git checkout"
 
 alias mr="make run"
+alias md="make debug"
+alias mc="make clean"
 
 . "$HOME/.cargo/env"
 
