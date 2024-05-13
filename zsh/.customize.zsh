13f7b60b0333cbd3579d5c571116278eae6dce38
Tunglies <tunglies@outlook.com>
2024-05-13 20:06:05 +0800

    system and git proxy

--- zsh/.customize.zsh
+++ zsh/.customize.zsh
@@ -1,15 +1,22 @@
 #!/bin/zsh
 
-if ip link show eth0 &> /dev/null; then
-    ip_address=$(ip addr show eth0 | awk '/inet / {print $2}' | cut -d'/' -f1)
-    gateway=$(ip route show dev eth0 | awk '/default via/ {print $3}')
-    if [[ "${ip_address%%.*}" == "172" ]]; then
-        proxy_gateway="${ip_address%.*}.1"
-    fi
+#if ip link show eth0 &> /dev/null; then
+#    ip_address=$(ip addr show eth0 | awk '/inet / {print $2}' | cut -d'/' -f1)
+#    gateway=$(ip route show dev eth0 | awk '/default via/ {print $3}')
+#    if [[ "${ip_address%%.*}" == "172" ]]; then
+#        proxy_gateway="${ip_address%.*}.1"
+#    fi
+#else
+#    proxy_gateway="127.0.0.1"
+#fi
+
+if cat /etc/resolv.conf &> /dev/null; then
+  proxy_gateway=$(cat /etc/resolv.conf |grep "nameserver" |cut -f 2 -d " ")
 else
-    proxy_gateway="127.0.0.1"
+  proxy_gateway="127.0.0.1"
 fi
 export all_proxy="http://$proxy_gateway:7890"
+git config --global all.proxy $all_proxy
 
 export PATH="/usr/sbin:$PATH";
 export PATH="/usr/lib/ccache/:$PATH"
