06fd337b30c9139ef74eb29398a0305d13d66009
Tunglies <tunglies@outlook.com>
2024-05-13 20:32:34 +0800

    better path export

--- zsh/.customize.zsh
+++ zsh/.customize.zsh
@@ -1,29 +1,33 @@
 #!/bin/zsh
 
-#if ip link show eth0 &> /dev/null; then
-#    ip_address=$(ip addr show eth0 | awk '/inet / {print $2}' | cut -d'/' -f1)
-#    gateway=$(ip route show dev eth0 | awk '/default via/ {print $3}')
-#    if [[ "${ip_address%%.*}" == "172" ]]; then
-#        proxy_gateway="${ip_address%.*}.1"
-#    fi
-#else
-#    proxy_gateway="127.0.0.1"
-#fi
+set_proxy() {
+  if cat /etc/resolv.conf &> /dev/null; then
+    proxy_gateway=$(cat /etc/resolv.conf |grep "nameserver" |cut -f 2 -d " ")
+  else
+    proxy_gateway="127.0.0.1"
+  fi
+  export all_proxy="http://$proxy_gateway:7890"
+  git config --global all.proxy $all_proxy
+}
+set_proxy
 
-if cat /etc/resolv.conf &> /dev/null; then
-  proxy_gateway=$(cat /etc/resolv.conf |grep "nameserver" |cut -f 2 -d " ")
-else
-  proxy_gateway="127.0.0.1"
-fi
-export all_proxy="http://$proxy_gateway:7890"
-git config --global all.proxy $all_proxy
-
-export PATH="/usr/sbin:$PATH";
-export PATH="/usr/lib/ccache/:$PATH"
-export PATH="/opt/riscv64-unknown-elf-gcc-8.3.0-2020.04.1-x86_64-linux-ubuntu14/bin:$PATH"
-export PATH="/opt/qemu-9.0.0/build:$PATH"
-export PATH="/opt/qemu-9.0.0/build/riscv64-sofmmu:$PATH"
-export PATH="/opt/qemu-9.0.0/build/rscv64-linux-user:$PATH"
+set_path() {
+    local paths=($@)  # Store the passed paths in an array
+    for _path in "${paths[@]}"; do
+        if [[ -d "$_path" && ":$PATH:" != *":$_path:"* ]]; then
+            export PATH="$_path:$PATH"
+        fi
+    done
+}
+custom_paths=(
+  "/usr/sbin"
+  "/usr/lib/ccache/"
+  "/opt/riscv64-unknown-elf-gcc-8.3.0-2020.04.1-x86_64-linux-ubuntu14/bin"  
+  "/opt/qemu-9.0.0/build"
+  "/opt/qemu-9.0.0/build/riscv64-sofmmu"
+  "/opt/qemu-9.0.0/build/rscv64-linux-user"
+)
+set_path "${custom_paths[@]}"
 
 #alias vim="nvim"
 alias cls="clear"
